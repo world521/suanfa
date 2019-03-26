b764992a3df7074565d03bd99bba6cbea9cfc788
fengqingsong <fengqingsong8@163.com>
2019-03-26 09:32:32 +0800

    Initial Commit

---
+++ SuanFa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SuanFa
+//
+//  Created by fengqingsong on 2019/3/26.
+//  Copyright © 2019 fqs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
