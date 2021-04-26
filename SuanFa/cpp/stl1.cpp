ad9080cde5bc5ba6164349cd33cdbe352a5a63e0
fengqingsong <fengqingsong8@163.com>
2021-04-26 17:35:24 +0800

    1

---
+++ SuanFa/cpp/stl1.cpp
@@ -0,0 +1,9 @@
+//
+//  stl1.cpp
+//  SuanFa
+//
+//  Created by fengqingsong on 2021/3/23.
+//  Copyright © 2021 fqs. All rights reserved.
+//
+
+#include "stl1.hpp"
---
+++ SuanFa/cpp/stl1.hpp
@@ -0,0 +1,14 @@
+//
+//  stl1.hpp
+//  SuanFa
+//
+//  Created by fengqingsong on 2021/3/23.
+//  Copyright © 2021 fqs. All rights reserved.
+//
+
+#ifndef stl1_hpp
+#define stl1_hpp
+
+#include <stdio.h>
+
+#endif /* stl1_hpp */
